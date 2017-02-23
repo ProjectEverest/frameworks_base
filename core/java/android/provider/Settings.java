7cd374fe019d07e64c289de7f6d896840bb382fc
maxwen <max.weninger@gmail.com>
2017-02-24 01:28:00 +0300

    SystemUI: config to hide status bar on lock screen [1/2]
    visibility can be toggled with tapping at the status bar
    
    AICPfied: brought up to O, 11 & 12 by eyosen
    
    BenzoEdit: Fixed system icons for Pie
    Change-Id: Ib05b4960f47bd65fb40122507cb7ea3d48b4d436
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7320,6 +7320,11 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_TOP_PADDING = "statusbar_top_padding";
+        
+        /**
+         * @hide
+         */
+        public static final String LOCK_HIDE_STATUS_BAR = "lockscreen_hide_status_bar";
 
         /**
          * Force full screen for devices with cutout
@@ -7485,6 +7490,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NAV_BAR_COMPACT_LAYOUT);
             PRIVATE_SETTINGS.add(NAVIGATION_BAR_IME_SPACE);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
+            PRIVATE_SETTINGS.add(LOCK_HIDE_STATUS_BAR);
         }
 
         /**
