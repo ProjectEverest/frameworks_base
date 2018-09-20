3f58c1b1887db5496e4964cd3ed0c22ea72ab312
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-20 15:10:37 +0530

    Clock: Statusbar Clock Customization [1/2] Add Clock size, color & font style.
    Clock fonts and size - @temasek
    Port to Oreo by @varund7726
    Port to Pie, Q, R & S + Added Color for Clocks - SuperDroidBond
    
    spkal01: Remove Clock size, Migrate to TunerService, Remove Unwanted Fonts
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6661,6 +6661,18 @@ public final class Settings {
         public static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
 
         /**
+         * Clock font color
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_COLOR = "status_bar_clock_color";
+
+        /**
+         * Clock font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
