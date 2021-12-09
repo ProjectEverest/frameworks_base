48c9c7c89774561b2823ffc0c19a70f8c8225a8e
spkal01 <spkal01@users.noreply.github.com>
2021-12-09 18:32:21 +0000

    SystemUI: Make app icons in statusbar optional [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6383,6 +6383,12 @@ public final class Settings {
         public static final String CHARGING_ANIMATION_BG = "charging_animation_bg";
 
 	/**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
