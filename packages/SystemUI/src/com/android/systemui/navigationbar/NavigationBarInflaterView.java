f7c06684631abf2ff724bab4c51fd28764ea7812
Alcatraz323 <alcatraz32323@gmail.com>
2022-04-20 21:54:44 +0800

    NavigationBarInflaterView: fix concurrent unsafe variable leads to nohint overlay reload forever
    * This variable can be accessed by multiple trigger below
    
    com.android.systemui.navigationbar.NavigationModeController$2.onReceive(NavigationModeController.java:114)
    com.android.systemui.navigationbar.NavigationBarInflaterView.onTuningChanged(NavigationBarInflaterView.java:224)
    
    Change-Id: I7df878653821dd641923c401e7ff3749c86c4ce8

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -55,6 +55,7 @@ import com.android.systemui.tuner.TunerService;
 import java.io.PrintWriter;
 import java.lang.ref.WeakReference;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class NavigationBarInflaterView extends FrameLayout
         implements TunerService.Tunable {
@@ -138,7 +139,7 @@ public class NavigationBarInflaterView extends FrameLayout
 
     private boolean mInverseLayout;
     private boolean mCompactLayout;
-    private boolean mIsHintEnabled;
+    private static AtomicReference<Boolean> mIsHintEnabledRef;
 
     public NavigationBarInflaterView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -148,6 +149,7 @@ public class NavigationBarInflaterView extends FrameLayout
         mNavBarMode = Dependency.get(NavigationModeController.class).addListener(mListener);
         mCompactLayout = Settings.System.getInt(context.getContentResolver(),
                                 Settings.System.NAV_BAR_COMPACT_LAYOUT, 0) != 0;
+        mIsHintEnabledRef = new AtomicReference<>(true);
     }
 
     @VisibleForTesting
@@ -194,7 +196,7 @@ public class NavigationBarInflaterView extends FrameLayout
                 defaultResource == R.string.config_navBarLayoutQuickstep) && mCompactLayout){
             return "left;back,home,recent;right";
         }
-        if (!mIsHintEnabled && defaultResource == R.string.config_navBarLayoutHandle) {
+        if (!mIsHintEnabledRef.get() && defaultResource == R.string.config_navBarLayoutHandle) {
             return getContext().getString(defaultResource).replace(HOME_HANDLE, "");
         }
         return getContext().getString(defaultResource);
@@ -233,7 +235,8 @@ public class NavigationBarInflaterView extends FrameLayout
                 setNavigationBarLayout(getDefaultLayout());
             }
         } else if (KEY_NAVIGATION_HINT.equals(key)) {
-            mIsHintEnabled = TunerService.parseIntegerSwitch(newValue, true);
+            Boolean mIsHintEnabledOld = mIsHintEnabledRef.get();
+            mIsHintEnabledRef.compareAndSet(mIsHintEnabledOld, TunerService.parseIntegerSwitch(newValue, true));
             updateHint();
             onLikelyDefaultLayoutChange();
         }
@@ -311,7 +314,7 @@ public class NavigationBarInflaterView extends FrameLayout
     private void updateHint() {
         final IOverlayManager iom = IOverlayManager.Stub.asInterface(
                 ServiceManager.getService(Context.OVERLAY_SERVICE));
-        final boolean state = mNavBarMode == NAV_BAR_MODE_GESTURAL && !mIsHintEnabled;
+        final boolean state = mNavBarMode == NAV_BAR_MODE_GESTURAL && !mIsHintEnabledRef.get();
         final int userId = ActivityManager.getCurrentUser();
         try {
             iom.setEnabled(OVERLAY_NAVIGATION_HIDE_HINT, state, userId);
