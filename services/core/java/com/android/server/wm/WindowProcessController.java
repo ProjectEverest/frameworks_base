a3a61ab6152869c2e3b36e30731799b17da9e591
tanxiaoyan <tanxiaoyan@xiaomi.com>
2023-08-05 15:08:55 +0800

    Update application info for activity record when application info changed
    Activity manager will schedule application info changed for process record and app thread When OMS#setEnabled is called.
    But the app process may be restarted for top-activity, it use application of the top activity which may not be updated.
    
    Bug:294624020
    
    Change-Id: I7fac9fab11c6f62350b31e486dcea89268bd086b
    Signed-off-by: tanxiaoyan <tanxiaoyan@xiaomi.com>

--- services/core/java/com/android/server/wm/WindowProcessController.java
+++ services/core/java/com/android/server/wm/WindowProcessController.java
@@ -117,7 +117,7 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
     private int mRapidActivityLaunchCount;
 
     // all about the first app in the process
-    final ApplicationInfo mInfo;
+    volatile ApplicationInfo mInfo;
     final String mName;
     final int mUid;
 
@@ -1723,10 +1723,15 @@ public class WindowProcessController extends ConfigurationContainer<Configuratio
             Configuration overrideConfig = new Configuration(r.getRequestedOverrideConfiguration());
             overrideConfig.assetsSeq = assetSeq;
             r.onRequestedOverrideConfigurationChanged(overrideConfig);
+            r.updateApplicationInfo(mInfo);
             if (r.isVisibleRequested()) {
                 r.ensureActivityConfiguration(0, true);
             }
         }
+    }
+
+    public void updateApplicationInfo(ApplicationInfo aInfo) {
+        mInfo = aInfo;
     }
 
     /**
