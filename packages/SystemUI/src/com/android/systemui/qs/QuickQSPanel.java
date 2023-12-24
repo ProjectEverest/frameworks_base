82bab611d59594f1827a1ba883a3894b49760b09
Pranav Vashi <neobuddy89@gmail.com>
2023-12-25 00:24:45 +0530

    SystemUI: Ensure updating margins and paddings on brightness slider
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -133,6 +133,7 @@ public class QuickQSPanel extends QSPanel implements TunerService.Tunable {
                 getPaddingTop(),
                 getPaddingEnd(),
                 bottomPadding);
+        setBrightnessViewMargin(mTop);
     }
 
     @Override
@@ -169,6 +170,11 @@ public class QuickQSPanel extends QSPanel implements TunerService.Tunable {
                 boolean value =
                         TunerService.parseInteger(newValue, 2) > 1;
                 super.onTuningChanged(key, value ? newValue : "0");
+                break;
+            case QS_BRIGHTNESS_SLIDER_POSITION:
+                mTop = TunerService.parseInteger(newValue, 0) == 0;
+                updatePadding();
+                super.onTuningChanged(key, newValue);
                 break;
             default:
                 super.onTuningChanged(key, newValue);
