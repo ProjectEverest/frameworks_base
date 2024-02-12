1149b5a2d3a945d424929c00c438b6557abd6f41
Dhina17 <dhinalogu@gmail.com>
2024-02-12 07:05:42 +0000

    PhoneWindowManager: Pass correct assistInvocationType for KEYCODE_ASSIST
    GTS expects correct invocation type on assistant launch.
    
    Change-Id: Ia73f4f7d5498ec9924844460c8717c8da068d16a

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3730,7 +3730,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             }
                             performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
                                     "Assist - Long Press");
-                            performKeyAction(mAssistLongPressAction, event);
+                            performKeyAction(mAssistLongPressAction, event,
+                                    AssistUtils.INVOCATION_TYPE_ASSIST_BUTTON);
                             mAssistPressed = false;
                         }
                     }
@@ -3741,7 +3742,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         }
                         mAssistPressed = false;
                         if (!canceled) {
-                            performKeyAction(mAssistPressAction, event);
+                            performKeyAction(mAssistPressAction, event,
+                                    AssistUtils.INVOCATION_TYPE_ASSIST_BUTTON);
                         }
                     }
                 }
