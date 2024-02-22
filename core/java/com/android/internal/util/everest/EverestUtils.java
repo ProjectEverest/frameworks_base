08ee7e4904b5e22649adad4e33a671fee603ee4d
Pranav Vashi <neobuddy89@gmail.com>
2024-02-22 09:54:41 +0000

    EverestUtils: Refractor isPackageInstalled method
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/com/android/internal/util/everest/EverestUtils.java
+++ core/java/com/android/internal/util/everest/EverestUtils.java
@@ -165,23 +165,22 @@ public class EverestUtils {
     }
 
     // Check to see if a package is installed
-    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
-        if (pkg != null) {
+    public static boolean isPackageInstalled(Context context, String packageName, boolean ignoreState) {
+        if (packageName != null) {
             try {
-                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
-                if (!pi.applicationInfo.enabled && !ignoreState) {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(packageName, 0);
+                if ((!pi.applicationInfo.enabled || !pi.applicationInfo.isProduct()) && !ignoreState) {
                     return false;
                 }
-            } catch (NameNotFoundException e) {
+            } catch (PackageManager.NameNotFoundException e) {
                 return false;
             }
         }
-
         return true;
     }
 
-    public static boolean isPackageInstalled(Context context, String pkg) {
-        return isPackageInstalled(context, pkg, true);
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        return isPackageInstalled(context, packageName, true);
     }
 
     // Check to see if device supports the Fingerprint scanner
