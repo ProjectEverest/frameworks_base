e960661ca74f9a6e92c603cc58e0fe313f8e6d48
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-28 13:09:16 +0300

    base: Add a dev option to unlimit screenrecord filesize [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6642,6 +6642,14 @@ public final class Settings {
         public static final String QS_SHOW_DATA_USAGE = "qs_show_data_usage";
 
         /**
+         * Whether to unlimit screenrecord filesize
+         * Not for backup - dev option
+         * @hide
+         */
+        @Readable
+        public static final String UNLIMIT_SCREENRECORD = "unlimit_screenrecord";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
