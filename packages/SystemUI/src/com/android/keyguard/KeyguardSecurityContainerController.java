d288dcf1fe4cff5ad7370d3e08faa1f842bf91fa
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-06 18:42:44 +0800

    SystemUI: Introduce Depth Wallpaper feature [1/2]
    * like ios wallpaper depth, taken from pixel xpert
    * todo: implement subject segmentation when we found a way to import all gms and ml kit dependencies
      aside from importing aars (note to other source maintainers: i tried and but the dependencies were too many so i dropped subject segmentation impl)
    
    Co-authored-by: Siavash <siavash@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainerController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainerController.java
@@ -804,6 +804,7 @@ public class KeyguardSecurityContainerController extends ViewController<Keyguard
         if (mCurrentSecurityMode != SecurityMode.None) {
             mView.startAppearAnimation(mCurrentSecurityMode);
             getCurrentSecurityController(controller -> controller.startAppearAnimation());
+            com.android.systemui.util.WallpaperDepthUtils.getInstance(getContext()).hideDepthWallpaper();
         }
     }
 
@@ -821,6 +822,7 @@ public class KeyguardSecurityContainerController extends ViewController<Keyguard
                                 onFinishRunnable);
                         if (!didRunAnimation && onFinishRunnable != null) {
                             onFinishRunnable.run();
+                            com.android.systemui.util.WallpaperDepthUtils.getInstance(getContext()).updateDepthWallpaperVisibility();
                         }
                     });
         }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -41,6 +41,7 @@ import static com.android.systemui.statusbar.StatusBarState.SHADE;
 import static com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout.ROWS_ALL;
 
 import android.annotation.Nullable;
+import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
 import android.app.IWallpaperManager;
 import android.app.KeyguardManager;
@@ -96,6 +97,7 @@ import android.view.IWindowManager;
 import android.view.MotionEvent;
 import android.view.ThreadedRenderer;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewConfiguration;
 import android.view.WindowInsets;
 import android.view.WindowManager;
@@ -250,6 +252,7 @@ import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
 import com.android.systemui.util.WallpaperController;
+import com.android.systemui.util.WallpaperDepthUtils;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.concurrency.MessageRouter;
 import com.android.systemui.util.kotlin.JavaAdapter;
@@ -470,6 +473,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
     private final Lazy<LightRevealScrimViewModel> mLightRevealScrimViewModelLazy;
+    
+    private final WallpaperDepthUtils mWallpaperDepthUtils;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -925,6 +930,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         if (predictiveBackSysui()) {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
+        mWallpaperDepthUtils = WallpaperDepthUtils.getInstance(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1172,6 +1178,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                 (requestTopUi, componentTag) -> mMainExecutor.execute(() ->
                         mNotificationShadeWindowController.setRequestTopUi(
                                 requestTopUi, componentTag))));
+                                
+		ViewGroup rootView = (ViewGroup) getNotificationShadeWindowView().findViewById(R.id.scrim_behind).getParent();
+		@SuppressLint("DiscouragedApi")
+		ViewGroup targetView = rootView.findViewById(mContext.getResources().getIdentifier("notification_container_parent", "id", mContext.getPackageName()));
+		targetView.addView(mWallpaperDepthUtils.getDepthWallpaperView(), 1);
     }
 
     @VisibleForTesting
@@ -2847,6 +2858,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            mWallpaperDepthUtils.updateDepthWallpaperVisibility();
         }
 
         /**
