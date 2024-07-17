b0c68ac16260bb7b24dd92b64ca1f29e145b0db1
elpaablo <donopwhile1@gmail.com>
2024-07-17 07:32:05 +0100

    Implement QS Split Shade (1/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7191,6 +7191,12 @@ public final class Settings {
         };
 
         /**
+         * Whether to enable qs split shade in landscape mode
+         * @hide
+         */
+        public static final String QS_SPLIT_SHADE = "qs_split_shade";
+
+        /**
          * These are all public system settings
          *
          * @hide
