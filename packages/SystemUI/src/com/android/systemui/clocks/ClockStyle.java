HinohArata/android_frameworks_base-rising

Clock Styles: Rework custom clock styles
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -1,0 +1,136 @@
+package com.android.systemui.clocks;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextClock;
+
+import com.android.internal.util.crdroid.ThemeUtils;
+import com.android.systemui.res.R;
+import com.android.systemui.Dependency;
+import com.android.systemui.tuner.TunerService;
+
+public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
+
+    private static final int[] CLOCK_LAYOUTS = {
+            0,
+            R.layout.keyguard_clock_oos,
+            R.layout.keyguard_clock_center,
+            R.layout.keyguard_clock_cos,
+            R.layout.keyguard_clock_custom,
+            R.layout.keyguard_clock_miui,
+            R.layout.keyguard_clock_ide,
+            R.layout.keyguard_clock_hyper,
+            R.layout.keyguard_clock_stylish,
+            R.layout.keyguard_clock_sidebar,
+            R.layout.keyguard_clock_minimal,
+            R.layout.keyguard_clock_minimal2
+    };
+
+    int[] centerClocks = {2, 4, 8, 9, 10, 11};
+
+    private static final int DEFAULT_STYLE = 0; // Disabled
+    private static final String CLOCK_STYLE_KEY = "clock_style";
+    private static final String CLOCK_STYLE = "system:" + CLOCK_STYLE_KEY;
+
+    private ThemeUtils mThemeUtils;
+    private Context mContext;
+    private View currentClockView;
+    private int mClockStyle;
+
+    private static final long UPDATE_INTERVAL_MILLIS = 15 * 1000;
+    private final Handler mHandler;
+    private long lastUpdateTimeMillis = 0;
+
+    public ClockStyle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mHandler = Dependency.get(Dependency.MAIN_HANDLER);
+        mThemeUtils = new ThemeUtils(context);
+        Dependency.get(TunerService.class).addTunable(this, CLOCK_STYLE);
+    }
+
+    private void updateClockOverlays() {
+        mThemeUtils.setOverlayEnabled(
+                "android.theme.customization.smartspace",
+                mClockStyle != 0 ? "com.android.systemui.hide.smartspace" : "com.android.systemui",
+                "com.android.systemui");
+        mThemeUtils.setOverlayEnabled(
+                "android.theme.customization.smartspace_offset",
+                mClockStyle != 0 && isCenterClock(mClockStyle)
+                        ? "com.android.systemui.smartspace_offset.smartspace"
+                        : "com.android.systemui",
+                "com.android.systemui");
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        updateClockView();
+    }
+
+    private void updateTextClockViews(View view) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View childView = viewGroup.getChildAt(i);
+                updateTextClockViews(childView);
+                if (childView instanceof TextClock) {
+                    ((TextClock) childView).refreshTime();
+                }
+            }
+        }
+    }
+
+    public void onTimeChanged() {
+        long currentTimeMillis = System.currentTimeMillis();
+        if (currentTimeMillis - lastUpdateTimeMillis >= UPDATE_INTERVAL_MILLIS) {
+            if (currentClockView != null) {
+                updateTextClockViews(currentClockView);
+                lastUpdateTimeMillis = currentTimeMillis;
+            }
+        }
+    }
+
+    private void updateClockView() {
+        if (currentClockView != null) {
+            ((ViewGroup) currentClockView.getParent()).removeView(currentClockView);
+            currentClockView = null;
+        }
+        if (mClockStyle > 0 && mClockStyle < CLOCK_LAYOUTS.length) {
+            ViewStub stub = findViewById(R.id.clock_view_stub);
+            if (stub != null) {
+                stub.setLayoutResource(CLOCK_LAYOUTS[mClockStyle]);
+                currentClockView = stub.inflate();
+                int gravity = isCenterClock(mClockStyle) ? Gravity.CENTER : Gravity.START;
+                if (currentClockView instanceof LinearLayout) {
+                    ((LinearLayout) currentClockView).setGravity(gravity);
+                }
+            }
+        }
+        updateClockOverlays();
+    }
+
+    private boolean isCenterClock(int clockStyle) {
+        for (int centerClock : centerClocks) {
+            if (centerClock == clockStyle) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        if (CLOCK_STYLE.equals(key)) {
+            mClockStyle = TunerService.parseInteger(newValue, 0);
+            updateClockView();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/weather/WeatherViewController.java
@@ -1,0 +1,183 @@
+/*
+ * Copyright (C) 2023-2024 risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.weather;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.provider.Settings;
+import android.view.View;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.internal.util.crdroid.OmniJawsClient;
+import com.android.systemui.res.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeatherViewController implements OmniJawsClient.OmniJawsObserver {
+
+    private Context mContext;
+    private OmniJawsClient mWeatherClient;
+    private OmniJawsClient.WeatherInfo mWeatherInfo;
+    private ImageView mWeatherImageView;
+    private TextView mWeatherTextView;
+    private SettingsObserver mSettingsObserver;
+
+    private boolean mShowWeatherLocation;
+    private boolean mShowWeatherText;
+    private boolean mWeatherEnabled;
+
+    private static final Map<String, Integer> WEATHER_CONDITIONS = new HashMap<>();
+
+    static {
+        WEATHER_CONDITIONS.put("clouds", R.string.weather_condition_clouds);
+        WEATHER_CONDITIONS.put("rain", R.string.weather_condition_rain);
+        WEATHER_CONDITIONS.put("clear", R.string.weather_condition_clear);
+        WEATHER_CONDITIONS.put("storm", R.string.weather_condition_storm);
+        WEATHER_CONDITIONS.put("snow", R.string.weather_condition_snow);
+        WEATHER_CONDITIONS.put("wind", R.string.weather_condition_wind);
+        WEATHER_CONDITIONS.put("mist", R.string.weather_condition_mist);
+    }
+
+    public WeatherViewController(Context context, ImageView weatherImageView, TextView weatherTextView) {
+        mContext = context;
+        mWeatherImageView = weatherImageView;
+        mWeatherTextView = weatherTextView;
+        mWeatherClient = new OmniJawsClient(context);
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+            mSettingsObserver.observe();
+        }
+    }
+
+    public void updateWeatherSettings() {
+        mWeatherEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_WEATHER_ENABLED,
+                0, UserHandle.USER_CURRENT) != 0;
+        mShowWeatherLocation = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_WEATHER_LOCATION,
+                0, UserHandle.USER_CURRENT) != 0;
+        mShowWeatherText = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_WEATHER_TEXT,
+                1, UserHandle.USER_CURRENT) != 0;
+        if (mWeatherImageView != null) {
+            mWeatherImageView.setVisibility(mWeatherEnabled ? View.VISIBLE : View.GONE);
+        }
+        if (mWeatherTextView != null) {
+            mWeatherTextView.setVisibility(mWeatherEnabled ? View.VISIBLE : View.GONE);
+        }
+        if (mWeatherEnabled) {
+            enableUpdates();
+        } else {
+            disableUpdates();
+        }
+    }
+
+    private void enableUpdates() {
+        if (mWeatherClient != null) {
+            mWeatherClient.addObserver(this);
+            queryAndUpdateWeather();
+        }
+    }
+
+    public void disableUpdates() {
+        if (mWeatherClient != null) {
+            mWeatherClient.removeObserver(this);
+        }
+    }
+
+    @Override
+    public void weatherError(int errorReason) {
+        if (errorReason == OmniJawsClient.EXTRA_ERROR_DISABLED) {
+            mWeatherInfo = null;
+            if (mWeatherImageView != null) {
+                mWeatherImageView.setImageDrawable(null);
+            }
+            if (mWeatherTextView != null) {
+                mWeatherTextView.setText("");
+            }
+        }
+    }
+
+    @Override
+    public void weatherUpdated() {
+        queryAndUpdateWeather();
+    }
+
+    private void queryAndUpdateWeather() {
+        try {
+            if (mWeatherClient == null || !mWeatherEnabled) {
+                return;
+            }
+            mWeatherClient.queryWeather();
+            mWeatherInfo = mWeatherClient.getWeatherInfo();
+            if (mWeatherInfo != null) {
+                if (mWeatherImageView != null) {
+                    Drawable d = mWeatherClient.getWeatherConditionImage(mWeatherInfo.conditionCode);
+                    mWeatherImageView.setImageDrawable(d);
+                }
+                if (mWeatherTextView != null) {
+                    String formattedCondition = mWeatherInfo.condition.toLowerCase();
+                    String conditionText = getConditionText(formattedCondition);
+                    mWeatherTextView.setText(mWeatherInfo.temp + " " + mWeatherInfo.tempUnits 
+                        + (mShowWeatherLocation ? " · " + mWeatherInfo.city : "") 
+                        + (mShowWeatherText ? " · " + conditionText : ""));
+                }
+            }
+        } catch (Exception e) {}
+    }
+
+    private String getConditionText(String condition) {
+        for (Map.Entry<String, Integer> entry : WEATHER_CONDITIONS.entrySet()) {
+            if (condition.contains(entry.getKey())) {
+                return mContext.getResources().getString(entry.getValue());
+            }
+        }
+        return condition;
+    }
+    
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_LOCATION), false, this,
+                    UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_TEXT), false, this,
+                    UserHandle.USER_ALL);
+            updateWeatherSettings();
+        }
+
+        void unobserve() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateWeatherSettings();
+        }
+    }
+}

ProjectEverest/frameworks_base

packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -1,0 +1,156 @@
+package com.android.systemui.clocks;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextClock;
+
+import com.android.internal.util.everest.ThemeUtils;
+import com.android.systemui.res.R;
+import com.android.systemui.Dependency;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
+import com.android.systemui.tuner.TunerService;
+
+public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
+
+    private static final int[] CLOCK_LAYOUTS = {
+            0,
+            R.layout.keyguard_clock_oos,
+            R.layout.keyguard_clock_center,
+            R.layout.keyguard_clock_cos,
+            R.layout.keyguard_clock_custom,
+            R.layout.keyguard_clock_miui,
+            R.layout.keyguard_clock_ide,
+            R.layout.keyguard_clock_hyper,
+            R.layout.keyguard_clock_stylish,
+            R.layout.keyguard_clock_sidebar,
+            R.layout.keyguard_clock_minimal,
+            R.layout.keyguard_clock_minimal2
+    };
+
+    int[] centerClocks = {2, 4, 8, 9, 10, 11};
+
+    private static final int DEFAULT_STYLE = 0; // Disabled
+    private static final String CLOCK_STYLE_KEY = "clock_style";
+    private static final String CLOCK_STYLE = "system:" + CLOCK_STYLE_KEY;
+    
+    private final KeyguardStateController mKeyguardStateController;
+
+    private ThemeUtils mThemeUtils;
+    private Context mContext;
+    private View currentClockView;
+    private int mClockStyle;
+
+    private static final long UPDATE_INTERVAL_MILLIS = 15 * 1000;
+    private final Handler mHandler;
+    private long lastUpdateTimeMillis = 0;
+    private boolean mIsOnKeyguard = false;
+
+    public ClockStyle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mHandler = Dependency.get(Dependency.MAIN_HANDLER);
+        mThemeUtils = new ThemeUtils(context);
+        Dependency.get(TunerService.class).addTunable(this, CLOCK_STYLE);
+        mKeyguardStateController = Dependency.get(KeyguardStateController.class);
+        mKeyguardStateController.addCallback(new KeyguardStateController.Callback() {
+            @Override
+            public void onKeyguardShowingChanged() {
+                mIsOnKeyguard = mKeyguardStateController.isShowing();
+            }
+            @Override
+            public void onKeyguardFadingAwayChanged() {
+                mIsOnKeyguard = false;
+            }
+            @Override
+            public void onKeyguardGoingAwayChanged() {
+                mIsOnKeyguard = false;
+            }
+        });
+    }
+
+    private void updateClockOverlays() {
+        mThemeUtils.setOverlayEnabled(
+                "android.theme.customization.smartspace",
+                mClockStyle != 0 ? "com.android.systemui.hide.smartspace" : "com.android.systemui",
+                "com.android.systemui");
+        mThemeUtils.setOverlayEnabled(
+                "android.theme.customization.smartspace_offset",
+                mClockStyle != 0 && isCenterClock(mClockStyle)
+                        ? "com.android.systemui.smartspace_offset.smartspace"
+                        : "com.android.systemui",
+                "com.android.systemui");
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        updateClockView();
+    }
+
+    private void updateTextClockViews(View view) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View childView = viewGroup.getChildAt(i);
+                updateTextClockViews(childView);
+                if (childView instanceof TextClock) {
+                    ((TextClock) childView).refreshTime();
+                }
+            }
+        }
+    }
+
+    public void onTimeChanged() {
+        if (!mIsOnKeyguard) return;
+        long currentTimeMillis = System.currentTimeMillis();
+        if (currentTimeMillis - lastUpdateTimeMillis >= UPDATE_INTERVAL_MILLIS) {
+            if (currentClockView != null) {
+                updateTextClockViews(currentClockView);
+                lastUpdateTimeMillis = currentTimeMillis;
+            }
+        }
+    }
+
+    private void updateClockView() {
+        if (currentClockView != null) {
+            ((ViewGroup) currentClockView.getParent()).removeView(currentClockView);
+            currentClockView = null;
+        }
+        if (mClockStyle > 0 && mClockStyle < CLOCK_LAYOUTS.length) {
+            ViewStub stub = findViewById(R.id.clock_view_stub);
+            if (stub != null) {
+                stub.setLayoutResource(CLOCK_LAYOUTS[mClockStyle]);
+                currentClockView = stub.inflate();
+                int gravity = isCenterClock(mClockStyle) ? Gravity.CENTER : Gravity.START;
+                if (currentClockView instanceof LinearLayout) {
+                    ((LinearLayout) currentClockView).setGravity(gravity);
+                }
+            }
+        }
+        updateClockOverlays();
+    }
+
+    private boolean isCenterClock(int clockStyle) {
+        for (int centerClock : centerClocks) {
+            if (centerClock == clockStyle) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        if (CLOCK_STYLE.equals(key)) {
+            mClockStyle = TunerService.parseInteger(newValue, 0);
+            updateClockView();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/weather/WeatherViewController.java
@@ -1,0 +1,182 @@
+/*
+ * Copyright (C) 2023-2024 risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.weather;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.provider.Settings;
+import android.view.View;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.internal.util.everest.OmniJawsClient;
+import com.android.systemui.res.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeatherViewController implements OmniJawsClient.OmniJawsObserver {
+
+    private Context mContext;
+    private OmniJawsClient mWeatherClient;
+    private OmniJawsClient.WeatherInfo mWeatherInfo;
+    private ImageView mWeatherImageView;
+    private TextView mWeatherTextView;
+    private SettingsObserver mSettingsObserver;
+
+    private boolean mShowWeatherLocation;
+    private boolean mShowWeatherText;
+    private boolean mWeatherEnabled;
+    private String mWeatherText;
+
+    private static final Map<String, Integer> WEATHER_CONDITIONS = new HashMap<>();
+
+    static {
+        WEATHER_CONDITIONS.put("clouds", R.string.weather_condition_clouds);
+        WEATHER_CONDITIONS.put("rain", R.string.weather_condition_rain);
+        WEATHER_CONDITIONS.put("clear", R.string.weather_condition_clear);
+        WEATHER_CONDITIONS.put("storm", R.string.weather_condition_storm);
+        WEATHER_CONDITIONS.put("snow", R.string.weather_condition_snow);
+        WEATHER_CONDITIONS.put("wind", R.string.weather_condition_wind);
+        WEATHER_CONDITIONS.put("mist", R.string.weather_condition_mist);
+    }
+
+    public WeatherViewController(Context context, ImageView weatherImageView, TextView weatherTextView, String weatherText) {
+        mContext = context;
+        mWeatherImageView = weatherImageView;
+        mWeatherTextView = weatherTextView;
+        mWeatherText = weatherText;
+        mWeatherClient = new OmniJawsClient(context);
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+            mSettingsObserver.observe();
+        }
+    }
+
+    public void updateWeatherSettings() {
+        mWeatherEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_WEATHER_ENABLED,
+                0, UserHandle.USER_CURRENT) != 0;
+        mShowWeatherLocation = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_WEATHER_LOCATION,
+                0, UserHandle.USER_CURRENT) != 0;
+        mShowWeatherText = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_WEATHER_TEXT,
+                1, UserHandle.USER_CURRENT) != 0;
+        if (mWeatherImageView != null) {
+            mWeatherImageView.setVisibility(mWeatherEnabled ? View.VISIBLE : View.GONE);
+        }
+        if (mWeatherTextView != null) {
+            mWeatherTextView.setVisibility(mWeatherEnabled || mWeatherText != null ? View.VISIBLE : View.GONE);
+        }
+        if (mWeatherEnabled) {
+            enableUpdates();
+        } else {
+            disableUpdates();
+        }
+    }
+
+    private void enableUpdates() {
+        if (mWeatherClient != null) {
+            mWeatherClient.addObserver(this);
+            queryAndUpdateWeather();
+        }
+    }
+
+    public void disableUpdates() {
+        if (mWeatherClient != null) {
+            mWeatherClient.removeObserver(this);
+        }
+    }
+
+    @Override
+    public void weatherError(int errorReason) {
+        if (errorReason == OmniJawsClient.EXTRA_ERROR_DISABLED) {
+            mWeatherInfo = null;
+            if (mWeatherImageView != null) {
+                mWeatherImageView.setImageDrawable(null);
+            }
+        }
+    }
+
+    @Override
+    public void weatherUpdated() {
+        queryAndUpdateWeather();
+    }
+
+    private void queryAndUpdateWeather() {
+        try {
+            if (mWeatherClient == null || !mWeatherEnabled) {
+                return;
+            }
+            mWeatherClient.queryWeather();
+            mWeatherInfo = mWeatherClient.getWeatherInfo();
+            if (mWeatherInfo != null) {
+                if (mWeatherImageView != null) {
+                    Drawable d = mWeatherClient.getWeatherConditionImage(mWeatherInfo.conditionCode);
+                    mWeatherImageView.setImageDrawable(d);
+                }
+                if (mWeatherTextView != null) {
+                    String formattedCondition = mWeatherInfo.condition.toLowerCase();
+                    String conditionText = getConditionText(formattedCondition);
+                    mWeatherTextView.setText(mWeatherInfo.temp + mWeatherInfo.tempUnits 
+                        + (mShowWeatherLocation ? " · " + mWeatherInfo.city : "") 
+                        + (mShowWeatherText ? " · " + conditionText : ""));
+                }
+            }
+        } catch (Exception e) {}
+    }
+
+    private String getConditionText(String condition) {
+        for (Map.Entry<String, Integer> entry : WEATHER_CONDITIONS.entrySet()) {
+            if (condition.contains(entry.getKey())) {
+                return mContext.getResources().getString(entry.getValue());
+            }
+        }
+        return condition;
+    }
+    
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_LOCATION), false, this,
+                    UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_TEXT), false, this,
+                    UserHandle.USER_ALL);
+            updateWeatherSettings();
+        }
+
+        void unobserve() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateWeatherSettings();
+        }
+    }
+}